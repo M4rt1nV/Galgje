ce7b9b390801e03619ff7fd04f4873eebe1e8695
M4rt1nV <martin_visser93@hotmail.com>
2023-09-22 11:27:39 +0200

    Added 2nd line for testing purposes

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     // Press Alt+Enter with your caret at the highlighted text to see how
     // IntelliJ IDEA suggests fixing it.
     System.out.println("Hello and welcome!");
-    System.out.println("Testtest");
+    System.out.println("Test test");
     // Press Shift+F10 or click the green arrow button in the gutter to run the code.
     for (int i = 1; i <= 5; i++) {
 
