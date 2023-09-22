32f307a75896597caad7746ca439a10b7c5c06aa
M4rt1nV <martin_visser93@hotmail.com>
2023-09-22 15:53:39 +0200

    Added in creation of the word to guess, and some tests to see if it was working

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@
 public class Main {
 
   public static void main(String[] args) {
-    // Press Alt+Enter with your caret at the highlighted text to see how
-    // IntelliJ IDEA suggests fixing it.
-    System.out.println("Hello and welcome!");
-    System.out.println("Test test");
-    // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+    MysteryWoord Raadwoord = new MysteryWoord();
+    System.out.println("Dit is het woord:");
+    System.out.println(Raadwoord.RaadWoord.toCharArray());
+    System.out.println(Raadwoord.BlancoWoord.toCharArray());
+    System.out.println("Dit was het woord");
+//    while (Raadwoord.BlancoWoord.contains(("_"))){
+//      //Heel veel spel spelen
+//    }
     
   }
 }
